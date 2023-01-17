27985bbb8571bc644fd8d657a9da4e7c92ef7d7b
filipp <louieandi@gmail.com>
2023-01-17 22:25:57 +0200

    Log support for android

--- engine/source/Steel/Core/Log.cpp
+++ engine/source/Steel/Core/Log.cpp
@@ -1,10 +1,23 @@
 #include "Log.h"
 
+#ifdef PLATFORM_ANDROID
+#include <spdlog/sinks/android_sink.h>
+#endif
+
+#ifdef PLATFORM_ANDROID
+std::shared_ptr<spdlog::logger> Log::_logger = nullptr;
+#endif
+
 void Log::Init()
 {
 #ifdef DISTRIBUTE_BUILD
     spdlog::set_level(spdlog::level::info);
 #else
     spdlog::set_level(spdlog::level::trace);
+#endif
+
+#ifdef PLATFORM_ANDROID
+    std::string tag = "AppSteel";
+    _logger = spdlog::android_logger_mt("Steel", tag);
 #endif
 }
--- engine/source/Steel/Core/Log.h
+++ engine/source/Steel/Core/Log.h
@@ -2,6 +2,10 @@
 
 #include <spdlog/spdlog.h>
 
+#ifdef PLATFORM_ANDROID
+#include <spdlog/sinks/android_sink.h>
+#endif
+
 class Log
 {
 public:
@@ -10,24 +14,45 @@ public:
     template<typename FormatString, typename... Args>
     inline static void LogDebug(const FormatString& fmt, Args&&... args)
     {
+#ifdef PLATFORM_ANDROID
+        _logger->debug(fmt, std::forward<Args>(args)...);
+#else
         spdlog::debug(fmt, std::forward<Args>(args)...);
+#endif
     }
 
     template<typename FormatString, typename... Args>
     inline static void LogInfo(const FormatString& fmt, Args&&... args)
     {
+#ifdef PLATFORM_ANDROID
+        _logger->info(fmt, std::forward<Args>(args)...);
+#else
         spdlog::info(fmt, std::forward<Args>(args)...);
+#endif
     }
 
     template<typename FormatString, typename... Args>
     inline static void LogWarning(const FormatString& fmt, Args&&... args)
     {
+#ifdef PLATFORM_ANDROID
+        _logger->warn(fmt, std::forward<Args>(args)...);
+#else
         spdlog::warn(fmt, std::forward<Args>(args)...);
+#endif
     }
 
     template<typename FormatString, typename... Args>
     inline static void LogError(const FormatString& fmt, Args&&... args)
     {
+#ifdef PLATFORM_ANDROID
+        _logger->error(fmt, std::forward<Args>(args)...);
+#else
         spdlog::error(fmt, std::forward<Args>(args)...);
+#endif
     }
+
+private:
+#ifdef PLATFORM_ANDROID
+    static std::shared_ptr<spdlog::logger> _logger;
+#endif
 };
